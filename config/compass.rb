54b5424e718c646a079cc73ea719aab41825b60c
John Francesco Ferlito <johnf@inodes.org>
2010-05-16 14:07:03 +1000

    Update pickle

--- config/compass.rb
+++ config/compass.rb
@@ -1,7 +1,7 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
-project_path = RAILS_ROOT if defined?(RAILS_ROOT)
+project_path = Rails.root if defined?(Rails.root)
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
