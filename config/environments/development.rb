b47f1877caa4e760cd28307af7f3a583079fb383
John Francesco Ferlito <johnf@inodes.org>
2010-05-16 13:26:41 +1000

    upgrade cucumber

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,5 +17,5 @@ Eopas::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
