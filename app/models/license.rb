6752e7f65378b4279e2694d00960b52a36c78a0b
Silvia Pfeiffer <silvia.pfeiffer@vquence.com>
2010-06-07 22:25:30 +1000

    Adopting AU 3.0 Creative Commons License, just hours after the AU government: http://www.freedomtodiffer.com/freedom_to_differ/2010/06/the-australian-parliament-goes-cc---with-v30.html ;)

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,12 +1,12 @@
 class License < Struct.new(:code, :name)
   LICENSES = {
     "PD"             => ["Public Domain", "http://creativecommons.org/publicdomain/"],
-    "CC-AU-BY"       => ["CC Attribution", "http://creativecommons.org/licenses/by/2.5/au/"],
-    "CC-AU-BY-SA"    => ["CC Attribution Share-alike", "http://creativecommons.org/licenses/by-sa/2.5/au/"],
-    "CC-AU-BY-ND"    => ["CC Attribution No derivative works", "http://creativecommons.org/licenses/by-nd/2.5/au/"],
-    "CC-AU-NC-BY"    => ["CC Noncommercial Attribution", "http://creativecommons.org/licenses/by-nc/2.5/au/"],
-    "CC-AU-NC-BY-SA" => ["CC Noncommercial Attribution Share-alike", "http://creativecommons.org/licenses/by-nc-sa/2.5/au/"],
-    "CC-AU-NC-BY-ND" => ["CC Noncommercial Attribution No derivative works", "http://creativecommons.org/licenses/by-nc-nd/2.5/au/"]
+    "CC-AU-BY"       => ["CC Attribution", "http://creativecommons.org/licenses/by/3.0/au/"],
+    "CC-AU-BY-SA"    => ["CC Attribution Share-alike", "http://creativecommons.org/licenses/by-sa/3.0/au/"],
+    "CC-AU-BY-ND"    => ["CC Attribution No derivative works", "http://creativecommons.org/licenses/by-nd/3.0/au/"],
+    "CC-AU-NC-BY"    => ["CC Noncommercial Attribution", "http://creativecommons.org/licenses/by-nc/3.0/au/"],
+    "CC-AU-NC-BY-SA" => ["CC Noncommercial Attribution Share-alike", "http://creativecommons.org/licenses/by-nc-sa/3.0/au/"],
+    "CC-AU-NC-BY-ND" => ["CC Noncommercial Attribution No derivative works", "http://creativecommons.org/licenses/by-nc-nd/3.0/au/"]
   }
 
     def self.find(code)
