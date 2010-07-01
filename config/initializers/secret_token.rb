1d396c930063b194fa6b31239e417ec90656596d
John Francesco Ferlito <johnf@inodes.org>
2010-07-02 01:12:29 +1000

    deprecation logging

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Rails.application.config.secret_token = 'ce3c748f7ea426e75f762f858577632cf91c6db532771352a45e8d2db9745f2043d9e7ba298f9c835801bd26aa18cf75696844f25891f301adcbcfb72c7b994b'
