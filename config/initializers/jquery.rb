981323167ad726ad51d8049ae1053a616d90175f
John Francesco Ferlito <johnf@inodes.org>
2010-08-16 23:13:48 +1000

    Fix jquery

--- config/initializers/jquery.rb
+++
@@ -1,6 +0,0 @@
-# Switch the javascript_include_tag :defaults to use jquery instead of
-# the default prototype helpers.
-Eopas::Application.configure do
-  config.action_view.javascript_expansions = { :defaults => ['jquery-1.4.2', 'rails'] }
-end
-
