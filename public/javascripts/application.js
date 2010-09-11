43047f208fccbc64fa3291144f56845d075d4b42
Silvia Pfeiffer <silvia.pfeiffer@vquence.com>
2010-09-11 18:45:05 +1000

    Some UI improvements.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// resizing the transcript_display div with the remaining screen-size
+function do_onResize() {
+  elem = $('#transcript_display');
+  if (!elem) return;
+  if ($.browser.msie) {
+    //y = document.body.clientHeight - 95;
+    x = $('body').offsetWidth - 380;
+    elem.width(x);
+    //elem.style.height = '700px';
+  } else if ($.browser.opera) {
+    // Opera is special: it doesn't like changing width
+    elem.height(window.innerHeight - 110);
+  } else {
+    elem.width(window.innerWidth - 395);
+    elem.height(window.innerHeight - 110);
+  }
+}
+
 $(document).ready(function() {
 
   $('.collapse_icon').mouseover(function() {
@@ -13,35 +31,8 @@ $(document).ready(function() {
     $(this).parent().next().slideToggle();
   });
 
-});
-
-$(window).load(function() {
   do_onResize();
-    $(window).resize(do_onResize);
+  $(window).resize(do_onResize);
 });
 
-function do_onResize() {
-  if ($.browser.msie) {
-    //y = document.body.clientHeight - 95;
-    x = document.body.offsetWidth - 380;
-    if (document.getElementById('transcripts_display')) {
-    document.getElementById('transcripts_display').style.width = x;
-    //document.getElementById('transcripts_display').style.height = '700px';
-    }
-  } else if ($.browser.opera) {
-    // Opera is special: it doesn't like calculating with variables on the width and height
-    // and it controls the width by itself just fine
-    if (document.getElementById('transcripts_display')) {
-      document.getElementById('transcripts_display').style.height = window.innerHeight - 110 + 'px';
-      document.getElementById('transcripts_display').style.overflow = 'scroll';
-    }
-  } else {
-    screenY = window.innerHeight - 110;
-    screenX = window.innerWidth - 395;
-    if (document.getElementById('transcripts_display')) {
-      document.getElementById('transcripts_display').style.width = screenX + 'px';
-      document.getElementById('transcripts_display').style.height = screenY + 'px';
-    }
-  }
-}
 
