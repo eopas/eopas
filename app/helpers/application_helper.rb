5f557744388f3012dd27fe4fdf18315feafaffe2
John Francesco Ferlito <johnf@inodes.org>
2011-01-03 20:10:46 +0100

    Make error messages html safe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
   def error_messages(object)
-    moo = ''
+    html = ''
     if object.errors.any?
-      moo = content_tag :div, :id => 'errorExplanation' do
+      html = content_tag :div, :id => 'errorExplanation' do
         content_tag :h2, 'The following errors prohibited this form from being saved:'
         content_tag :ul do
           li = ''
           object.errors.full_messages.each do |msg|
-            li << content_tag(:li, msg)
+            li << content_tag(:li, msg.html_safe)
           end
-          li
+          li.html_safe
         end
       end
     end
-    moo
+    html.html_safe
   end
 end
