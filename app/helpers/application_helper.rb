12329fc396fa850d173874b6b78a89cee7657fb2
Silvia Pfeiffer <silvia.pfeiffer@vquence.com>
2011-01-13 16:52:38 +0100

    Some more consistency on the table ordering and search.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,6 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == params[:sort]) ? "current #{params[:direction]}" : nil
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
   end
 end
