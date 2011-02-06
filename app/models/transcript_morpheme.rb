9f87fc2bbd82025789bd199ada72e7a29e6e32da
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2011-02-06 12:37:14 +1100

    Allowing empty morpheme and gloss fields.

--- app/models/transcript_morpheme.rb
+++ app/models/transcript_morpheme.rb
@@ -2,6 +2,4 @@ class TranscriptMorpheme < ActiveRecord::Base
   belongs_to :word, :class_name => :transcript_word
 
   validates :position, :presence => true, :numericality => true
-  validates :morpheme, :presence => true
-  validates :gloss,    :presence => true
 end
