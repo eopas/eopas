eaf54f351ae7458420324bb4c5ebc411136837ad
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2011-02-07 15:03:24 +1100

    Participant dependency on transcript fails when transcript is new. Also added a creator role to participants.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,7 +2,7 @@ class Participant < ActiveRecord::Base
   belongs_to :transcript
 
   ROLES = %w(
-    annotator artist author compiler consultant data_inputter depositor
+    annotator artist author compiler consultant creator data_inputter depositor
     developer editor illustrator interviewer participant performer
     photographer recorder researcher respondent speaker signer singer sponsor
     transcriber translator
@@ -10,6 +10,6 @@ class Participant < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :role, :inclusion => {:in => ROLES}
-  validates :transcript, :associated => true
+  validates :transcript, :presence => true
 
 end
