ceb995b8aa9e916310c94eb4df4c15b5e2b79c79
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2011-04-02 17:28:31 +1100

    trying to fix the environment setting for deploying delayed_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,14 +30,14 @@ after "deploy:restart", "delayed_job:restart"
 namespace :delayed_job do
  desc "Stop the delayed_job process"
  task :stop, :roles => :app do
-   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job stop"
+   run "cd #{current_path} && RAILS_ENV=#{environment} script/delayed_job stop"
  end
  desc "Start the delayed_job process"
  task :start, :roles => :app do
-   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job start"
+   run "cd #{current_path} && RAILS_ENV=#{environment} script/delayed_job start"
  end
  desc "Restart the delayed_job process"
  task :restart, :roles => :app do
-   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job restart"
+   run "cd #{current_path} && RAILS_ENV=#{environment} script/delayed_job restart"
  end
 end
