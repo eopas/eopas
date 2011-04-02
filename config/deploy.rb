5bedef4b178410ce770fcdbf2ec5a39cb6fc5d9b
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2011-04-02 17:25:36 +1100

    Making delayed_jobs rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,9 +22,22 @@ namespace :deploy do
   end
 end
 
-# Delayed job needs this
-set :rails_env, 'production'
 # Delayed job
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
+
+namespace :delayed_job do
+ desc "Stop the delayed_job process"
+ task :stop, :roles => :app do
+   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job stop"
+ end
+ desc "Start the delayed_job process"
+ task :start, :roles => :app do
+   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job start"
+ end
+ desc "Restart the delayed_job process"
+ task :restart, :roles => :app do
+   run "cd #{current_path} && RAILS_ENV=#{ENV['DEPLOY']} script/delayed_job restart"
+ end
+end
